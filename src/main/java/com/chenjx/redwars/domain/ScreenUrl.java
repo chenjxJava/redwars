f0620ab23bc999341be6739ca6fc68a7ac518c6e
chenjxJava <chenjx_java@163.com>
2018-12-27 11:59:10 +0800

    vue

---
+++ src/main/java/com/chenjx/redwars/domain/ScreenUrl.java
@@ -0,0 +1,36 @@
+package com.chenjx.redwars.domain;
+
+/**
+ * create by chenjx 2018/12/24
+ */
+public class ScreenUrl {
+    private Long id;
+
+    private String name;
+
+    private String url;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
